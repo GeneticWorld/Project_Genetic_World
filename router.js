0c03f27edfa836e12dfb8bf8487418175e664d4e
unknown <Aprendiz@ITAAPRCSRFSD157.FormacionSena.RED>
2023-02-27 16:59:40 -0500

    guardado1

--- router.js
+++ router.js
@@ -5,4 +5,12 @@ router.get('/regi', (req, res) => {
     res.render('registro.ejs')
 })
 
+router.get('/log', (req, res) => {
+    res.render('login.ejs')
+})
+
+router.get('/regiCita', (req, res) => {
+    res.render('formularioAgendamiento.ejs')
+})
+
 module.exports = router;
